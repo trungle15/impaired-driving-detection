59c8bd2f26df1cbce2758ddcb0d0942f17956757
trungle15 <50526463+trungle15@users.noreply.github.com>
2023-07-03 15:50:35 -0500

    fix data_splitter bugs

--- utils/data_splitter.py
+++ utils/data_splitter.py
@@ -8,19 +8,6 @@ from os.path import join
 import utils
 from utils.constants import METADATA_DIR
 
-# def train_val_split_indices(
-#         labels, person_IDs, val_size, n_splits, 
-#         random_state=None, stratified_fold=False, person_ID=False):
-
-#     split_kwargs = {'n_splits': n_splits, 'test_size': val_size, 'random_state': random_state}
-#     split_func, split_args = (GroupShuffleSplit, {'X': np.zeros_like(person_IDs), 'y': labels, 'groups': person_IDs}) if person_ID \
-#                             else (StratifiedShuffleSplit if stratified_fold else ShuffleSplit, {'X': np.zeros_like(labels), 'y': labels})
-
-#     split = split_func(**split_kwargs)
-#     indices = list(split.split(**split_args))
-
-#     return indices
-
 def train_val_split_indices(labels, n_splits, val_size=0.3, random_state=None, stratified_fold=False, person_ID=False, person_IDs=None):
     if person_ID and person_IDs is None:
         raise ValueError("person_IDs must be supplied when person_ID=True")
