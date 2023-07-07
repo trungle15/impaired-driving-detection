f23f88c7876eb9e3f457d2b3ddb2e153232beb9f
trungle15 <50526463+trungle15@users.noreply.github.com>
2023-07-07 13:28:46 -0500

    fix groups

--- utils/data_provider.py
+++ utils/data_provider.py
@@ -43,22 +43,26 @@ def split_features_targets(df, return_tensor = False):
 
     return features, targets
         
-def load_interstate_data(paradigm, return_feature_target = True,  return_tensor = None, return_group = False):
+
+def convert_to_L(indices):
+    return [(L(index[0].tolist()), L(index[1].tolist())) for index in indices]
+
+def load_interstate_data(paradigm, return_feature_target=True, return_tensor=None, return_group=False):
     if paradigm == 'overlap':
         df = pd.read_csv(OVERLAP_SAMPLES)
     elif paradigm == 'non_overlap':
         df = pd.read_csv(NON_OVERLAP_SAMPLES)
     else:
-        raise KeyError("Please use 'non_overlap' or 'overlap")
+        raise KeyError("Please use 'non_overlap' or 'overlap'")
     
     df = clean_unnamed(df)
+    
     if return_feature_target:
-        features, targets = split_features_targets(df, return_tensor= return_tensor)
+        features, targets = split_features_targets(df, return_tensor=return_tensor)
         if return_group:
-            return features, targets, df['Subject'].to_numpy()
+            # Get the group for each unique sample
+            groups = df.drop_duplicates('Non_Overlap_Sample')['Subject'].to_numpy()
+            return features, targets, groups
         return features, targets
     else: 
         return df
-
-def convert_to_L(indices):
-    return [(L(index[0].tolist()), L(index[1].tolist())) for index in indices]
