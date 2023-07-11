0375b6bb4f8b37d420e472c65406d7aaf6a4bf08
trungle15 <50526463+trungle15@users.noreply.github.com>
2023-07-11 16:32:43 -0500

    update saving mechanism

--- models/trainer.py
+++ models/trainer.py
@@ -24,10 +24,10 @@ def create_dls_list(index, X, y, tfms, batch_tfms):
         dls.append(dl)
     return dls
 
-# For now there is no mechanism to pass random_state to this function, so this must be 
+# For now there is no mechanism to pass random_state and paradigm to this function, so this must be 
 # done manually
-def CNN_train_val_loop(archs, dls, epochs, random_state=None, save_results = False):
-    results = pd.DataFrame(columns=['arch', 'hyperparams', 'total params', 'train loss', 'valid loss', 'accuracy', 'f1_score', 'auc', 'precision', 'recall', 'specificity' , 'time'])
+def CNN_train_val_loop(archs, dls, epochs, paradigm=None, random_state=None, save_results = False):
+    results = pd.DataFrame(columns=['arch', 'train loss', 'valid loss', 'accuracy', 'f1_score', 'auc', 'precision', 'recall', 'specificity' , 'time'])
     for i, (arch, k) in enumerate(archs):
         model = create_model(arch, dls=dls[i], **k)  # Use the ith dataloader
         print(model.__class__.__name__)
@@ -36,11 +36,11 @@ def CNN_train_val_loop(archs, dls, epochs, random_state=None, save_results = Fal
         learn.fit_one_cycle(epochs, 1e-3)  # Reduce number of epochs to 10, as per your last code block
         elapsed = time.time() - start
         vals = learn.recorder.values[-1]
-        results.loc[i] = [arch.__name__, k, count_parameters(model), vals[0], vals[1], vals[2], vals[3], vals[4], int(elapsed)]
+        results.loc[i] = [arch.__name__, vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7], int(elapsed)]
         clear_output()
         display(results)
     if save_results:
-        filename = f'{model.__class__.__name__}_{epochs}e_{random_state}rs_results'
+        filename = f'{model.__class__.__name__}_{paradigm}_{epochs}e_{random_state}rs_results.csv'
         filepath = join(RESULTS_DIR, filename)
         results.to_csv(filepath)
 
