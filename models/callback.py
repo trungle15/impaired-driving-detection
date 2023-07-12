2ed14b44c792f7bc0856bd96e55d419d7f6132c7
trungle15 <50526463+trungle15@users.noreply.github.com>
2023-07-12 17:16:35 -0500

    add custom callback to save predicted probas

---
+++ models/callback.py
@@ -0,0 +1,10 @@
+from fastai.callback.core import Callback
+import pandas as pd
+from tsai.all import *
+
+class SavePredsCallback(Callback):
+    def after_epoch(self):
+        preds, targs = self.learn.get_X_preds(X=self.dls.valid.items)
+        pred_probs, pred_classes = preds.max(dim=1)
+        pred_df = pd.DataFrame({'sample_index': self.dls.valid.items, 'model_id': self.learn.model_idx, 'pred_proba': pred_probs, 'pred_class': pred_classes})
+        pred_df.to_csv(f'{self.learn.model.__class__.__name__}_{self.learn.paradigm}_{self.learn.epochs}e_{self.learn.random_state}rs_preds_{self.learn.epoch}.csv', index=False)
--- models/trainer.py
+++ models/trainer.py
@@ -1,5 +1,6 @@
 import tsai
 from tsai.all import *
+from fastai.callback.tracker import EarlyStoppingCallback
 
 import utils
 from utils.data_provider import load_interstate_data, convert_to_L
@@ -11,6 +12,7 @@ from os.path import join
 
 import models
 from models.metrics import Fastai_Specificity
+from models.callback import SavePredsCallback
 
 def create_dls_list(index, X, y, tfms, batch_tfms):
     index_L = convert_to_L(index)
@@ -26,14 +28,18 @@ def create_dls_list(index, X, y, tfms, batch_tfms):
 
 # For now there is no mechanism to pass random_state and paradigm to this function, so this must be 
 # done manually
+
+### This section is for CNN-based
 def CNN_train_val_loop(archs, dls, epochs, paradigm=None, random_state=None, save_results = False):
     results = pd.DataFrame(columns=['arch', 'train loss', 'valid loss', 'accuracy', 'f1_score', 'auc', 'precision', 'recall', 'specificity' , 'time'])
     for i, (arch, k) in enumerate(archs):
         model = create_model(arch, dls=dls[i], **k)  # Use the ith dataloader
         print(model.__class__.__name__)
-        learn = Learner(dls[i], model, metrics=[accuracy, F1Score(), RocAucBinary(), Precision(), Recall(), Fastai_Specificity()])  # Use the ith dataloader
+        learn = Learner(dls[i], model, metrics=[accuracy, F1Score(), RocAucBinary(), Precision(), Recall(), Fastai_Specificity()],
+                        cbs=[EarlyStoppingCallback(monitor='accuracy', min_delta=0.01, patience=3), SavePredsCallback()])  # Use the ith dataloader
         start = time.time()
-        learn.fit_one_cycle(epochs, 1e-3)  # Reduce number of epochs to 10, as per your last code block
+        lr = learn.lr_find(show_plot=False)
+        learn.fit_one_cycle(epochs, lr)  # Reduce number of epochs to 10, as per your last code block
         elapsed = time.time() - start
         vals = learn.recorder.values[-1]
         results.loc[i] = [arch.__name__, vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7], int(elapsed)]
@@ -44,6 +50,12 @@ def CNN_train_val_loop(archs, dls, epochs, paradigm=None, random_state=None, sav
         filename = f'{model.__class__.__name__}_{paradigm}_{epochs}e_{random_state}rs_results.csv'
         filepath = join(RESULTS_DIR, filename)
         results.to_csv(filepath)
+  
+
+        
+# def _save_result():
+#     _mkdirs_if_not_exist(RESULTS_DIR)
+#     filename = 
 
     
 
