f2f4af4d813edca3c41b79628b778ee9242a6fdc
trungle15 <50526463+trungle15@users.noreply.github.com>
2023-07-14 13:42:07 -0500

    callback file not working

--- models/callback.py
+++ models/callback.py
@@ -8,3 +8,5 @@ class SavePredsCallback(Callback):
         pred_probs, pred_classes = preds.max(dim=1)
         pred_df = pd.DataFrame({'sample_index': self.dls.valid.items, 'model_id': self.learn.model_idx, 'pred_proba': pred_probs, 'pred_class': pred_classes})
         pred_df.to_csv(f'{self.learn.model.__class__.__name__}_{self.learn.paradigm}_{self.learn.epochs}e_{self.learn.random_state}rs_preds_{self.learn.epoch}.csv', index=False)
+
+# Doesn't work
